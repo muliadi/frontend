bb52ce26a8c75440c663bc66f102b2639be42710
Ariel Keselman <skariel@gmail.com>
2016-04-07 14:37:59 +0300

    itemcreatecard is now functional

--- app/components/ItemCreateCard.js
+++ app/components/ItemCreateCard.js
@@ -1,64 +1,51 @@
 // /* jshint esversion: 6*/
 
-import React from 'react';
-
-class ItemCreateCard extends React.Component {
-    componentDidMount() {
-        this.setState({
-            name: "",
-            short_desc: "",
-        });
+const ItemCreateCard = (props) => {
+    var name;
+    var short_desc;
+    const handleName = function (event) {
+        name = event.target.value;
+        console.log(name)
     }
-    render() {
-        var handleName = function (event) {
-            this.setState({name: event.target.value});
-            console.log(this.state.name)
-        }.bind(this);
-        
-        var handleShortDesc = function (event) {
-            this.setState({short_desc: event.target.value});
-            console.log(this.state.short_desc)
-        }.bind(this);
-        
-        var style_card = {
-            width: "100%;",
-            maxWidth: "320px;",
-            height: "320px;",
-            background: "url('data:image;base64,"+this.props.image+"') center / cover",        
-            flexDirection: "column-reverse;",
-            marginLeft: "auto;",        
-            marginRight: "auto;",
-            paddingRight:"10px;",
-        };
-        return (
-            <div style={style_card}>
-                <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-                    <input
-                        className="mdl-textfield__input"
-                        type="text"
-                        id="sample3"
-                        onChange={handleName}></input>
-                    <label className="mdl-textfield__label" for="sample3">שם המוצר</label>
-                </div>
-                <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-                    <input
-                        className="mdl-textfield__input"
-                        type="text"
-                        id="sample4"
-                        onChange={handleShortDesc}></input>
-                    <label className="mdl-textfield__label" for="sample4">תאור קצר</label>
-                </div>
-                <button className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
-                    הוסף תמונה
-                </button>
-                <button className="mdl-button mdl-js-button mdl-js-ripple-effect">
-                    הוסף תמונה
-                </button>                                
+    const handleShortDesc = function (event) {
+        short_desc = event.target.value;
+        console.log(short_desc)
+    }
+    const style_card = {
+        width: "100%;",
+        maxWidth: "320px;",
+        height: "320px;",
+        flexDirection: "column-reverse;",
+        marginLeft: "auto;",        
+        marginRight: "auto;",
+        paddingRight:"10px;",
+    };
+    return (
+        <div style={style_card}>
+            <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                <input
+                    className="mdl-textfield__input"
+                    type="text"
+                    id="sample3"
+                    onChange={handleName}></input>
+                <label className="mdl-textfield__label" for="sample3">שם המוצר</label>
             </div>
-            
-            
-        );
-    }
+            <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                <input
+                    className="mdl-textfield__input"
+                    type="text"
+                    id="sample4"
+                    onChange={handleShortDesc}></input>
+                <label className="mdl-textfield__label" for="sample4">תאור קצר</label>
+            </div>
+            <button className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+                הוסף תמונה
+            </button>
+            <button className="mdl-button mdl-js-button mdl-js-ripple-effect">
+                הוסף תמונה
+            </button>                                
+        </div> 
+    );
 }
 
 export default ItemCreateCard;
