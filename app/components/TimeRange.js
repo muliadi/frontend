63f950fc5dbb9852a72f8401352dd51fa0de051c
Ariel Keselman <skariel@gmail.com>
2016-04-27 23:29:18 +0300

    clicking day in timerange works

--- app/components/TimeRange.js
+++ app/components/TimeRange.js
@@ -9,20 +9,28 @@ class TimeRange extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            checked : this.props.checked
+            checked : this.props.checked,
+            id : "timeRange_"+this.props.dayNum,
+        }
+    }
+    componentDidMount() {
+        componentHandler.upgradeDom();
+        if (this.state.checked) {
+            document.getElementById(this.state.id).click();
         }
     }
     render() {
         const dayName = ["ו", "ה", "ד", "ג", "ב", "א"][this.props.dayNum]
+        
         return (
-            <div style={{display:"flex", flexDirection:"row", borderBottom:"dotted rgba(0,0,0,0.1) 1px", height: "30px", marginTop:"7px"}}>
-                <label style={{width:"50px"}} className="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect" htmlFor="checkbox-1">
-                    <input type="checkbox" className="mdl-checkbox__input" />
+            <div style={{display:"flex", flexDirection:"row", borderBottom:"dotted rgba(0,0,0,0.1) 1px", height: "35px", marginTop:"7px"}}>
+                <label style={{width:"50px"}} className="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect" htmlFor={this.state.id}>
+                    <input id={this.state.id} type="checkbox" className="mdl-checkbox__input" />
                     <span className="mdl-checkbox__label">{dayName}</span>
                 </label>
                 
                 <div style={{width:"100%", marginTop:"3px"}}>
-                    <ReactSlider min="0" max="71" defaultValue={[25, 57]} withBars />
+                    <ReactSlider min={0} max={71} defaultValue={[25, 57]} withBars />
                 </div>
             </div>
         );
