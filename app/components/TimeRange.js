80226dc03be2a7bc260ed6943b516e7677b5bae2
Ariel Keselman <skariel@gmail.com>
2016-04-28 23:49:03 +0300

    correctly changing state in restaurantcreatecard

--- app/components/TimeRange.js
+++ app/components/TimeRange.js
@@ -35,12 +35,12 @@ class TimeRange extends React.Component {
         return this.getTimeVal(this.state.left)
     }
     render() {
+        const onChange = ()=>{this.props.onChange(this.props.dayNum, this.state.checked, this.getTimeFrom(), this.getTimeTo())}
         const dayName = ["ו", "ה", "ד", "ג", "ב", "א"][this.props.dayNum]
-        this.props.onChange(this.props.dayNum, this.state.checked, this.getTimeFrom(), this.getTimeTo())
         return (
             <div style={{display:"flex", flexDirection:"row", borderBottom:"dotted rgba(0,0,0,0.1) 1px", height: "35px", marginTop:"7px"}}>
                 <label style={{width:"70px"}} className="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect" htmlFor={this.state.id}>
-                    <input id={this.state.id} type="checkbox" className="mdl-checkbox__input" onChange={(e)=>{this.setState({checked: e.target.checked})}}/>
+                    <input id={this.state.id} type="checkbox" className="mdl-checkbox__input" onChange={(e)=>{this.setState({checked: e.target.checked}, onChange)}}/>
                     <span className="mdl-checkbox__label">{dayName}</span>
                 </label>
                 
@@ -48,7 +48,7 @@ class TimeRange extends React.Component {
                     {this.getTimeFrom()}
                 </div>
                 <div style={{width:"100%", marginTop:"6px"}}>
-                    <ReactSlider min={0} max={72} defaultValue={[this.state.left, this.state.right]} onChange={(value)=>{this.setState({left: value[0], right:value[1]})}} withBars />
+                    <ReactSlider min={0} max={72} defaultValue={[this.state.left, this.state.right]} onChange={(value)=>{this.setState({left: value[0], right:value[1]}, onChange)}} withBars />
                 </div>
                 <div style={{width:"80px", marginTop:"3px", marginRight:"5px"}}>
                     {this.getTimeTo()}
