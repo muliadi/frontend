86fab7de9d0f9950d3ae3e051eb63ec757184a53
Ariel Keselman <skariel@gmail.com>
2016-04-30 21:21:39 +0300

    adding getmdl_select, updating npm packages

--- app/app.js
+++ app/app.js
@@ -7,15 +7,16 @@ import viewRoute from './routes/view_route.js';
 
 import Main from './components/Main.js'
 
+let mountNode = document.getElementById('app');
+let rootComponent = <Relay.RootContainer
+    Component={Main}
+    route={new viewRoute() } />;
+
 Relay.injectNetworkLayer(
     new Relay.DefaultNetworkLayer(window.location.origin + '/graphql', {
         credentials: 'same-origin',
     })
 );
 
-let mountNode = document.getElementById('app');
-let rootComponent = <Relay.RootContainer
-    Component={Main}
-    route={new viewRoute() } />;
 ReactDOM.render(rootComponent, mountNode);
 
