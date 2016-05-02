cd9c6145db36dbc08d7465fac764eb31c8a85b3b
Ariel Keselman <skariel@gmail.com>
2016-05-03 01:25:34 +0300

    itemcard is no stateful

--- app/components/ItemCard.js
+++ app/components/ItemCard.js
@@ -1,125 +1,132 @@
 /* jshint esversion: 6*/
 
 var __idnum = 0
-const ItemCard = ({name, image_id, price, amount, unitsName, vendor_image_id}) => {
-    const style_card_title = {
-        background: "url('/static/content/"+image_id+"') center / cover",
-        cursor: "pointer",                
+class ItemCard extends React.Component {
+    componentDidMount() {
+        componentHandler.upgradeDom();
     }
-    const style_card = {
-        width: "100%",
-        maxWidth: "200px",
-        height: "300px",
-        marginLeft: "auto",        
-        marginRight: "auto",        
-    };
-    const style_support_text = {
-        cursor: "pointer",
-    }
-    const style_name = {
-    };
-    const style_price = {
-    };
-    const style_units = {
-    };  
-    const style_vendor_logo = {
-        background: "url('/static/content/"+vendor_image_id+"') center / cover",
-        width: "20px",
-        height: "20px",
-    };  
-    const style_add_to_basket_button = {  
-        //background: "rgb(51, 172, 113)",
-    };
-    const style_amount_text = {
-        textAlign: "center",
-        marginTop: "10px",
-        contentEditable: "true",
-    }
-    const style_actions = {
-            display: "flex",
-            flexDirection: "row-reverse",
-            justifyContent: "space-between",            
-    };  
-    const getParentCard = (e)=>{
-        while (e.className!="mdl-card mdl-shadow--8dp") {
-            e = e.parentElement;
+    render() {
+        const style_card_title = {
+            background: "url('/static/content/"+this.props.image_id+"') center / cover",
+            cursor: "pointer",                
         }
-        return e;
-    }
-    const style_card_mouse_over = (e)=>{
-        //getParentCard(e.target).style.border = "solid red 3px";
-        getParentCard(e.target).style.boxShadow = "0px 0px 10px 2px rgb(51, 172, 113)"
-    }
-    const style_card_mouse_out = (e)=>{
-        //getParentCard(e.target).style.border = "solid red 3px";
-        getParentCard(e.target).style.boxShadow = "0 8px 10px 1px rgba(0,0,0,.14),0 3px 14px 2px rgba(0,0,0,.12),0 5px 5px -3px rgba(0,0,0,.2)"
-    }    
-    const __id = "itemcard_id_"+__idnum
-    __idnum += 1
-    return (
-        <div
-            className="mdl-card mdl-shadow--8dp"
-            onMouseOver={style_card_mouse_over}
-            onMouseOut={style_card_mouse_out}
-            style={style_card}>
-
-            <div className="mdl-tooltip mdl-tooltip--top" htmlFor={__id} >
-                הוסף אחד
-            </div>
-            <div className="mdl-tooltip mdl-tooltip--top" htmlFor={__id+"_1"}>
-                הורד אחד
-            </div>
-            <div className="mdl-tooltip mdl-tooltip--top" htmlFor={__id+"_2"}>
-                סה״כ בסל
-            </div>
-            
-             <div style={{backgroundImage:"url('/static/content/"+vendor_image_id+"')",backgroundPosition: "bottom right", backgroundRepeat: "no-repeat", backgroundSize: "60px", display: "inline-block", height: "20px", width: "100%", marginRight: "5px", marginBottom: "5px"}}>           
-           </div>
-            
-            
+        const style_card = {
+            width: "100%",
+            maxWidth: "200px",
+            height: "300px",
+            marginLeft: "auto",        
+            marginRight: "auto",        
+        };
+        const style_support_text = {
+            cursor: "pointer",
+        }
+        const style_name = {
+        };
+        const style_price = {
+        };
+        const style_units = {
+        };  
+        const style_vendor_logo = {
+            background: "url('/static/content/"+this.props.vendor_image_id+"') center / cover",
+            width: "20px",
+            height: "20px",
+        };  
+        const style_add_to_basket_button = {  
+            //background: "rgb(51, 172, 113)",
+        };
+        const style_amount_text = {
+            textAlign: "center",
+            marginTop: "10px",
+            contentEditable: "true",
+        }
+        const style_actions = {
+                display: "flex",
+                flexDirection: "row-reverse",
+                justifyContent: "space-between",            
+        };  
+        const getParentCard = (e)=>{
+            while (e.className!="mdl-card mdl-shadow--8dp") {
+                e = e.parentElement;
+            }
+            return e;
+        }
+        const style_card_mouse_over = (e)=>{
+            //getParentCard(e.target).style.border = "solid red 3px";
+            getParentCard(e.target).style.boxShadow = "0px 0px 10px 2px rgb(51, 172, 113)"
+        }
+        const style_card_mouse_out = (e)=>{
+            //getParentCard(e.target).style.border = "solid red 3px";
+            getParentCard(e.target).style.boxShadow = "0 8px 10px 1px rgba(0,0,0,.14),0 3px 14px 2px rgba(0,0,0,.12),0 5px 5px -3px rgba(0,0,0,.2)"
+        }    
+        const __id = "itemcard_id_"+__idnum
+        __idnum += 1
+        return (
             <div
-                className="mdl-card__title mdl-card--expand"
-                style={style_card_title}>
-            </div>
-            
-            <div className="mdl-card__supporting-text  mdl-card--border"
-                    style={style_support_text}>
-                <div>
-                    <div style={style_name}>
-                        {name}
-                    </div>
-                    <div style={style_price}>
-                        {amount} {unitsName} , {price} &#8362;  
-                    </div>
-                </div>
-            </div>
-            
-            <div className="mdl-card__actions mdl-card--border" style={style_actions}>
-                <button
-                    style={style_add_to_basket_button}
-                    id={__id}
-                    className="mdl-button mdl-js-button mdl-button--primary"
-                    >
-                    <i className="material-icons">add</i>
-                </button>
+                className="mdl-card mdl-shadow--8dp"
+                onMouseOver={style_card_mouse_over}
+                onMouseOut={style_card_mouse_out}
+                style={style_card}>
 
-                <div style={style_amount_text}  id={__id+"_2"}>
-                0
+                <div className="mdl-tooltip mdl-tooltip--top" htmlFor={__id} >
+                    הוסף אחד
+                </div>
+                <div className="mdl-tooltip mdl-tooltip--top" htmlFor={__id+"_1"}>
+                    הורד אחד
+                </div>
+                <div className="mdl-tooltip mdl-tooltip--top" htmlFor={__id+"_2"}>
+                    סה״כ בסל
                 </div>
                 
+                <div style={{backgroundImage:"url('/static/content/"+this.props.vendor_image_id+"')",backgroundPosition: "bottom right", backgroundRepeat: "no-repeat", backgroundSize: "60px", display: "inline-block", height: "20px", width: "100%", marginRight: "5px", marginBottom: "5px"}}>           
+            </div>
                 
-                <button
-                    style={style_add_to_basket_button}
-                    id={__id+"_1"}
-                    className="mdl-button mdl-js-button mdl-button--primary"
-                    >
-                    <i className="material-icons">remove</i>
-                </button>
                 
-            </div>        
-        </div>
-        
-    );    
+                <div
+                    className="mdl-card__title mdl-card--expand"
+                    style={style_card_title}>
+                </div>
+                
+                <div className="mdl-card__supporting-text  mdl-card--border"
+                        style={style_support_text}>
+                    <div>
+                        <div style={style_name}>
+                            {this.props.name}
+                        </div>
+                        <div style={style_price}>
+                            {this.props.amount} {this.props.unitsName} , {this.props.price} &#8362;  
+                        </div>
+                    </div>
+                </div>
+                
+                <div className="mdl-card__actions mdl-card--border" style={style_actions}>
+                    <button
+                        style={style_add_to_basket_button}
+                        id={__id}
+                        className="mdl-button mdl-js-button mdl-button--primary"
+                        >
+                        <i
+                            className="material-icons"
+                        >add</i>
+                    </button>
+
+                    <div style={style_amount_text}  id={__id+"_2"}>
+                    0
+                    </div>
+                    
+                    
+                    <button
+                        style={style_add_to_basket_button}
+                        id={__id+"_1"}
+                        className="mdl-button mdl-js-button mdl-button--primary"
+                        >
+                        <i className="material-icons">remove</i>
+                    </button>
+                    
+                </div>        
+            </div>
+            
+        ); 
+    }   
 }
 
 // TODO: add propTypes
