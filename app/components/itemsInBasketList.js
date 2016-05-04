54a8a4f04c7ab5d16667e3e67e590b2b5d596028
Ariel Keselman <skariel@gmail.com>
2016-05-04 21:26:36 +0300

    better formatting of code for itemsinbasket view

--- app/components/itemsInBasketList.js
+++ app/components/itemsInBasketList.js
@@ -17,27 +17,21 @@ class ItemsInBasketListSub extends React.Component {
         return (
             <div>
                 <div>
-                <div className="mdl-grid">
-                                  
-                                      
-                                        <div className="mdl-cell mdl-cell--5-col-desktop mdl-cell--5-col-tablet mdl-cell--4-col-phone">
-                                          <h6>  סל הקניות שלי</h6>
-                                        </div>
-                                         <div className="mdl-cell mdl-cell--5-col-desktop mdl-cell--5-col-tablet mdl-cell--4-col-phone">
-                                            <button className="mdl-button mdl-js-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" style={{ marginRight:"0px",  maxHeight: "25px", lineHeight:"11px", fontSize:"12px"}}>נקה סל
-                                                <i className="material-icons">delete_forever</i>
-                                            </button>
-                                        </div>
-                                        
-                                        <div className="mdl-cell mdl-cell--2-col-desktop mdl-cell--2-col-tablet mdl-cell--2-col-phone" >
-                                          <img src="/static/shopping-cart.png" alt="Shopping Cart" style={{height:"30px"}}></img>
-                                        </div> 
-                                                                
-                                </div>
-                               
-                    
-
-                   
+                    <div className="mdl-grid">
+                        <div className="mdl-cell mdl-cell--5-col-desktop mdl-cell--5-col-tablet mdl-cell--4-col-phone">
+                            <h6>  סל הקניות שלי</h6>
+                        </div>
+                            <div className="mdl-cell mdl-cell--5-col-desktop mdl-cell--5-col-tablet mdl-cell--4-col-phone">
+                            <button className="mdl-button mdl-js-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" style={{ marginRight:"0px",  maxHeight: "25px", lineHeight:"11px", fontSize:"12px"}}>נקה סל
+                                <i className="material-icons">delete_forever</i>
+                            </button>
+                        </div>
+                        
+                        <div className="mdl-cell mdl-cell--2-col-desktop mdl-cell--2-col-tablet mdl-cell--2-col-phone" >
+                            <img src="/static/shopping-cart.png" alt="Shopping Cart" style={{height:"30px"}}></img>
+                        </div> 
+                                                        
+                    </div>
                 </div> 
                 <ul className="mdl-list" style={style_list}>                
                     {
