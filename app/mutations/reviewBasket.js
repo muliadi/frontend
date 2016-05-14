0a09b66acb897e90c7444e38905039e32ebc4ede
Ariel Keselman <skariel@gmail.com>
2016-05-14 12:56:44 +0300

    accepting baskets works

---
+++ app/mutations/reviewBasket.js
@@ -0,0 +1,38 @@
+/* jshint esversion: 6 */
+
+import Relay from 'react-relay';
+
+export default class ReviewBasketMutation extends Relay.Mutation {
+    getMutation() {
+        return Relay.QL`mutation {reviewBasket}`;
+    }
+    getVariables() {
+        const v = {
+            basketID: this.props.basketID,
+            reviewStatus: this.props.reviewStatus,
+            reviewComment: this.props.reviewComment,
+        };
+        return v
+    }
+    getFatQuery() {
+        return Relay.QL`
+      fragment on reviewBasketPayload @relay(pattern: true) {
+          view {
+              me {
+                  baskets
+              }
+          }
+      }
+    `;
+    }
+    getConfigs() {
+        return [{
+            type: 'FIELDS_CHANGE',
+            fieldIDs: {
+                view: 'view',
+            }
+        }];
+    }
+}
+
+
